b075955423d5d526201cfd47336b0185af5c2f55
Nolan <92731646+Blood-NT@users.noreply.github.com>
2023-02-12 01:03:34 +0700

    ok
    ok

---
+++ AutoControlAppPC/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace AutoControlAppPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button17 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(300, 165);
+            this.button17.Margin = new System.Windows.Forms.Padding(4);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(199, 28);
+            this.button17.TabIndex = 23;
+            this.button17.Text = "Xuất ra tọa độ hình";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 464);
+            this.Controls.Add(this.button17);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button17;
+    }
+}
+
---
+++ AutoControlAppPC/Form1.cs
@@ -0,0 +1,203 @@
+﻿using KAutoHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoControlAppPC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void openapp(object sender, EventArgs e)
+        {
+            //Process.Start(AppDomain.CurrentDomain.BaseDirectory + "How Kteam - Free Education.html");
+            Process.Start("How Kteam - Free Education.html");
+        }
+        private void okk( int x, int y)
+        {
+            EMouseKey mouseKey = EMouseKey.LEFT;
+
+            AutoControl.MouseClick(x, y, mouseKey);
+        }
+
+        private void pingg()
+        {
+            Thread.Sleep(600);
+
+            AutoControl.SendKeyFocus(KeyCode.KEY_S);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_O);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_W);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_N);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_D);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_D);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_E);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_P);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_J);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_T);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_R);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_A);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_I);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_Q);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_U);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_A);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_S);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.ENTER);
+            Thread.Sleep(100);
+
+            AutoControl.SendKeyFocus(KeyCode.KEY_T);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_H);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_I);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_C);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_H);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_S);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_S);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_O);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_W);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_N);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_Q);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_U);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_A);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_S);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.ENTER);
+            Thread.Sleep(100);
+
+
+
+            AutoControl.SendKeyFocus(KeyCode.KEY_M);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_O);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_A);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_H);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_H);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_H);
+            Thread.Sleep(100);
+
+            AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+            Thread.Sleep(100); AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+            Thread.Sleep(100); AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+            Thread.Sleep(100); AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+            Thread.Sleep(100); AutoControl.SendKeyFocus(KeyCode.SPACE_BAR);
+            Thread.Sleep(100);
+
+
+
+            AutoControl.SendKeyFocus(KeyCode.KEY_M);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_O);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_A);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_H);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_H);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.KEY_H);
+            Thread.Sleep(100);
+            AutoControl.SendKeyFocus(KeyCode.ENTER);
+            Thread.Sleep(100);
+        }
+        private void coverr(string input)
+        {
+            
+            int a, b;
+
+            MatchCollection matches = Regex.Matches(input, @"\d+");
+            a = int.Parse(matches[0].Value);
+            b = int.Parse(matches[1].Value);
+            okk(a, b);
+        }
+
+        private void img(string tmp)
+        {
+
+            var screen = CaptureHelper.CaptureScreen();
+            screen.Save("mainScreen.PNG");
+            var subBitmap = ImageScanOpenCV.GetImage(tmp);
+
+
+            var resBitmap = ImageScanOpenCV.FindOutPoint((Bitmap)screen, subBitmap);
+            if (resBitmap != null)
+            {
+                coverr(resBitmap.ToString());
+
+            }
+
+        }
+        private void button17_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.facebook.com/trongsonne/";
+            System.Diagnostics.Process.Start(url);
+
+            Thread.Sleep(4000);
+
+            img("pick_mess.png");
+            Thread.Sleep(100);
+
+            img("tesstt.png");
+            Thread.Sleep(100);
+
+            pingg();
+           
+           
+                
+        }
+
+    }
+}
